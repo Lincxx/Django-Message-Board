e06cc34eaa7a6bd37875a8bb8abf3687f36e78a8
Jeremy Lincoln <jeremy.lincoln@biscorp.local>
2019-06-06 10:03:10 -0400

    Added in some testing

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Post
 
 # Create your tests here.
+class PostModelTest(TestCase):
+
+    def setUp(self):
+        Post.objects.create(text='just a test')
+
+    def text
+
